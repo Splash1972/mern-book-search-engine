ea8494013a22482e5f0d548d5f978bab053e3c2f
Splash1972 <161398773+Splash1972@users.noreply.github.com>
2024-07-30 16:35:34 -0400

    adding files and modifying code

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -103,7 +103,7 @@ const SearchBooks = () => {
                 />
               </Col>
               <Col xs={12} md={4}>
-                <Button type='submit' variant='success' size='lg'>
+                <Button type='submit' variant='info' size='lg'>
                   Submit Search
                 </Button>
               </Col>
